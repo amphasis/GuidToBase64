b57ae01a88384d7d4fe553cba50a75b82d96df9e
Lev Hobotov <l.hobotov@smartcat.ai>
2021-12-14 17:36:12 +0300

    Implement hex converter

---
+++ Converters/HexToBinaryConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace GuidToBase64.Converters
+{
+	public class HexToBinaryConverter : IConverter
+	{
+		public string InputTypeName => "Hex";
+		public string OutputTypeName => "MongoDB binary";
+
+		public string? TryParseInput(string input)
+		{
+			var match = _guidRegex.Match(input);
+			if (!match.Success) return null;
+
+			var guid = new Guid(Convert.FromHexString(match.Value));
+			return $"BinData(3, '{Convert.ToBase64String(guid.ToByteArray())}')";
+		}
+
+		private readonly Regex _guidRegex = new (@"[0-9A-Fa-f]{32}");
+	}
+}
