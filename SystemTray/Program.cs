7ab9d74ba13ef097bd58c2178d95875a93f2b809
Lev Khobotov <l.hobotov@smartcat.ai>
2024-02-26 10:55:41 +0300

    Add system tray icon

--- SystemTray/Program.cs
+++ SystemTray/Program.cs
@@ -1,17 +1,33 @@
-namespace MongoConverter.SystemTray
+namespace MongoConverter.SystemTray;
+
+internal static class Program
 {
-	internal static class Program
+	/// <summary>
+	///  The main entry point for the application.
+	/// </summary>
+	[STAThread]
+	static void Main()
 	{
-		/// <summary>
-		///  The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
+		// To customize application configuration such as set high DPI settings or default font,
+		// see https://aka.ms/applicationconfiguration.
+		ApplicationConfiguration.Initialize();
+
+		using var icon = new NotifyIcon();
+		icon.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+
+		icon.ContextMenuStrip = new ContextMenuStrip
 		{
-			// To customize application configuration such as set high DPI settings or default font,
-			// see https://aka.ms/applicationconfiguration.
-			ApplicationConfiguration.Initialize();
-			Application.Run(new Form1());
-		}
+			Items =
+			{
+				new ToolStripMenuItem("Show form", null, (s, e) => { new Form1().Show(); }),
+				new ToolStripMenuItem("Exit", null, (s, e) => { Application.Exit(); }),
+			}
+		};
+
+		icon.Visible = true;
+
+		Application.Run(new Form1());
+
+		icon.Visible = false;
 	}
 }
